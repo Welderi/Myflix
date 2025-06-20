b2146b9a65eb131c0384300eab023e7315fefc11
Kyriienko Yelyzaveta <lizakirienko007@gmail.com>
2025-06-20 10:59:44 +0200

    Creating new project

---
+++ apps/api/MyflixAPI/MyflixAPI/Controllers/MyflixController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyflixAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MyflixController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("hello");
+        }
+    }
+}
